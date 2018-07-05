cf53e91ec40c027fd529e92b7060d5ca2d54ead3
Robert Njane <robertnjane@MacBook-Pro-5.local>
2018-07-05 16:46:31 +0300

    feat(UserAuth): Implement User Authentication - User Register - User Login - User Logout

--- fapp/urls.py
+++ fapp/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'login.html'}, name='logout'),
     url(r'^register/$', views.register, name='register'),
     url(r'^', views.index, name='index'),
     url(r'^budget-detail/$', views.budget, name='budget'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'login.html'}, name='logout'),
 ]
